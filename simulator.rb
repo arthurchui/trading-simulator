077a0e5b7d0534610be147ca311d05d78043d0e0
John Zwiep <john.zwiep@clio.com>
2020-02-13 13:38:38 -0800

    rough pass of simulation logic

--- simulator.rb
+++ simulator.rb
@@ -1,27 +1,43 @@
 #!/usr/bin/env ruby
 
 require 'csv'
+require './position.rb'
 
 class Simulator
 
   def call
-    # TODO: Process data
-    # for each day
-    #    do we have an open position?
-    #      do we need to sell?
-    #    (else) do we intend to buy?
-    #      get open price
-    #      calculate stop loss
-    #      record
+    closed_positions = []
+    open_position = nil
+    pricing_data.each do |day|
+      current_date = day['Date']
+      if open_position
+        puts "#{day['Low']}, #{open_position.stop_loss_price}"
+        if day['Low'].to_f < open_position.stop_loss_price
+          open_position.close!(current_date)
+          closed_positions << open_position
+          open_position = nil
+        else
+          # TODO: update stop loss
+        end
+      elsif buy_dates.any? { |buy_date| buy_date['date'] == current_date }
+        open_position = Position.new(current_date, day['Open'], day['Open'].to_f * 0.99)  # TODO roundoff?
+      end
+    end
+
+    if open_position
+      open_position.close!(pricing_data[-1]['Date'], pricing_data[-1]['Close'])
+      closed_positions << open_position
+    end
+
     output_trade_history
   end
 
   def buy_dates
-    @buy_dates ||= CSV.read('data/tsla_buy_dates.csv', headers: true)
+    @buy_dates ||= CSV.read('data/nflx_buy_dates.csv', headers: true)
   end
 
   def pricing_data
-    @pricing_data ||= CSV.read('data/tsla_pricing_data.csv', headers: true)
+    @pricing_data ||= CSV.read('data/nflx_pricing_data.csv', headers: true)
   end
 
   def output_trade_history
